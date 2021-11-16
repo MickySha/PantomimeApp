a45a9bbd7d14fe77d27c7425797ed19282d8744a
mohammad sharifpour <mohammad.sharifpour.88@gmail.com>
2021-11-16 12:08:09 +0330

    Add: compelete StartFragment & Fix: gradle and dependency version fixed

--- app/src/main/java/com/micky/pantomim/ui/start/StartFragment.kt
+++ app/src/main/java/com/micky/pantomim/ui/start/StartFragment.kt
@@ -5,13 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.micky.pantomim.R
 import com.micky.pantomim.databinding.FragmentStartBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -30,6 +28,19 @@ class StartFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStart.setOnClickListener(View.OnClickListener {
+
+            openGameFragment()
+        })
+    }
+
+    private fun openGameFragment() {
+
+        findNavController().navigate(R.id.action_startFragment_to_gameFragment)
+    }
 
 
 }
