e166d388790de4674ad1ce5338ff135ce68f8001
mohammad sharifpour <mohammad.sharifpour.88@gmail.com>
2021-11-18 19:47:37 +0330

    Add: in GameFragment -> wrongAnswerFun() & correctAnswerFun() & increaseScore() & onClickFun() & nextWord() & getWords ()

--- app/src/main/java/com/micky/pantomim/ui/game/GameFragment.kt
+++ app/src/main/java/com/micky/pantomim/ui/game/GameFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.micky.pantomim.R
+import com.micky.pantomim.databinding.FragmentGameBinding
 
 
 /**
@@ -15,13 +15,118 @@ import com.micky.pantomim.R
  */
 class GameFragment : Fragment() {
 
+    lateinit var binding: FragmentGameBinding
+    var myWord: String = ""
+    lateinit var wordList: MutableList<String>
+    var counter =0
+    var score = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = FragmentGameBinding.inflate(inflater)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        wordList= getWords ()
+
+        onClickFun()
+        nextWord()
+    }
+
+    private fun onClickFun() {
+
+        binding.btnAddWord.setOnClickListener(View.OnClickListener {
+            addNewWordFun()
+        })
+
+        binding.btnStartTimer.setOnClickListener(View.OnClickListener {
+            startTimerFun()
+        })
+
+        binding.btnCorrect.setOnClickListener(View.OnClickListener {
+            correctAnswerFun()
+        })
+
+        binding.btnWrong.setOnClickListener(View.OnClickListener {
+            wrongAnswerFun()
+        })
+    }
+
+    private fun startTimerFun() {
+        //TODO("Not yet implemented")
+    }
+
+    private fun stopTimerFun() {
+        //TODO("Not yet implemented")
+    }
+
+    private fun addNewWordFun() {
+       //TODO("Not yet implemented")
+    }
+
+    private fun wrongAnswerFun() {
+        nextWord()
+        stopTimerFun()
+    }
+
+    private fun correctAnswerFun() {
+        nextWord()
+        stopTimerFun()
+
+        if (counter <= (wordList.size) -1) {
+
+            increaseScore()
+        }
+    }
+
+    private fun increaseScore() {
+        score++
+        binding.tvScore.text = score.toString()
+    }
+
+    private fun nextWord() {
+        if (counter <= (wordList.size) -1) {
+            myWord = wordList[counter]
+            counter ++
+
+        }
+
+        binding.tvPantomimeWord.text = myWord
+    }
+
+    private fun getWords(): MutableList<String> {
+
+        return mutableListOf(
+            "Lake",
+            "Sea",
+            "Ocean",
+            "Nature",
+            "Assistant",
+            "Access",
+            "Delivery",
+            "Grammar",
+            "Luxury",
+            "Fake",
+            "Pizza Tower",
+            "Plankton",
+            "Timer",
+            "Sticker",
+            "Comment",
+            "Confirm",
+            "Duel",
+            "Farm",
+            "Tractor",
+            "Track",
+            "Pace",
+            "Clockwise",
+            "Microbiology"
+        )
+    }
 }
